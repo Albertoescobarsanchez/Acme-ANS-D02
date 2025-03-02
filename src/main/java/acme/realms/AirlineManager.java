425adcc0a1b9ebd7ba44bb950bedcbc7f9740109
Alberto <albescsan1@alum.us.es>
2025-03-02 19:09:40 +0100

    airline manager

---
+++ src/main/java/acme/realms/AirlineManager.java
@@ -0,0 +1,41 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.components.basis.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AirlineManager extends AbstractRole {
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Pattern(regexp = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifierNumber;
+
+	private int					yearsOfExperience;
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birthDate;
+
+	@URL
+	private String				pictureLink;
+}
