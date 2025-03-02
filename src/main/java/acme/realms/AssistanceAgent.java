1a6e3780679f99753daa8662ea8f7b1372084d5b
Alberto <albescsan1@alum.us.es>
2025-03-02 20:58:38 +0100

    agent pattern fix

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -1,53 +1,51 @@
+
 package acme.realms;
 
 import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import acme.client.components.validation.Mandatory;
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.validation.Mandatory;
 import acme.entities.airline.Airline;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Entity
 @Getter
 @Setter
 public class AssistanceAgent extends AbstractRole {
 
-    private static final long	serialVersionUID	= 1L;
-    
-    @Mandatory
-    @Pattern(regexp = "^[A-Z]{2-3}\\d{6}$") 
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Pattern(regexp = "^[A-Z]{2,3}\\d{6}$")
 	@Column(unique = true)
-    private String employeeCode;
+	private String				employeeCode;
 
-    @Mandatory
-    @Size(max = 255)
-    private String languages;
+	@Mandatory
+	@Size(max = 255)
+	private String				languages;
 
-    @Mandatory
-    private Airline airline;
+	@Mandatory
+	private Airline				airline;
 
-    @Mandatory
-    @Past
-    private LocalDate startDate;
+	@Mandatory
+	@Past
+	private LocalDate			startDate;
 
-    @Size(max = 255)
-    private String bio;
+	@Size(max = 255)
+	private String				bio;
 
-    private Double salary;
+	private Double				salary;
 
-    @URL
-    private String photoURL;
-
+	@URL
+	private String				photoURL;
 
 }
