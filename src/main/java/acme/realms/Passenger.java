d31c7fd4fe271ac65d31cefa43143767b2bbb3ee
Olga Cantalejo <2004olgacg@gmail.com>
2025-03-04 10:02:40 +0100

    Update student02-Task005

--- src/main/java/acme/realms/Passenger.java
+++ src/main/java/acme/realms/Passenger.java
@@ -4,14 +4,17 @@ package acme.realms;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Past;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,24 +25,35 @@ import lombok.Setter;
 
 public class Passenger extends AbstractRole {
 
-	@Mandatory
-	@ValidString
-	private String fullName;
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@Email
-	private String email;
-	
+	@ValidString
+	@Automapped
+	private String				fullName;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
 	@Mandatory
 	@Pattern(regexp = "^[A-Z0-9]{6,9}$")
-	private String passportNumber;
-	
+	@Automapped
+	private String				passportNumber;
+
 	@Mandatory
-	@Past
-	private Date birthDate;
-	
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birthDate;
+
 	@Optional
 	@Size(max = 50)
-	private String specialNeeds;
-	
+	@Automapped
+	private String				specialNeeds;
+
 }
