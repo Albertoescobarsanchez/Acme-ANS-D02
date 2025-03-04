27ceed2a1db64a0924d496cfee2cc9117395da56
Alberto <albescsan1@alum.us.es>
2025-03-04 10:22:37 +0100

    aircraft update

--- src/main/java/acme/entities/airport/Aircraft.java
+++ src/main/java/acme/entities/airport/Aircraft.java
@@ -3,13 +3,17 @@ package acme.entities.airport;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Optional;
+import acme.entities.airline.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,21 +28,28 @@ public class Aircraft extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 	@NotBlank
 	@Size(max = 50)
-	private String	model;
+	private String				model;
 
 	@NotBlank
 	@Column(unique = true)
 	@Size(max = 50)
-	String			registrationNumber;
-	private int		capacityPassengers;
+	String						registrationNumber;
+
+	@Min(value = 0)
+	private int					capacityPassengers;
 
 	@Max(value = 50000)
 	@Min(value = 2000)
-	private int		cargoWeight;
+	private int					cargoWeight;
 
-	private boolean	status; //active or in maintenance
+	private boolean				status; //active or in maintenance
 
 	@Optional
 	@Size(max = 255)
-	private String	details;
+	private String				details;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 }
