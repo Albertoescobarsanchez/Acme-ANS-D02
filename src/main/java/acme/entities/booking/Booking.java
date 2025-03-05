5c01061ae4110efc26962d60678321d6cfa77186
Olga Cantalejo <2004olgacg@gmail.com>
2025-03-05 17:56:06 +0100

    student02-Task015

--- src/main/java/acme/entities/booking/Booking.java
+++ src/main/java/acme/entities/booking/Booking.java
@@ -2,13 +2,17 @@
 package acme.entities.booking;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
 import acme.client.components.basis.AbstractEntity;
@@ -18,6 +22,8 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
+import acme.realms.Customer;
+import acme.realms.Passenger;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -56,4 +62,13 @@ public class Booking extends AbstractEntity {
 	@Automapped
 	private String				lastNibble;
 
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Customer			customer;
+
+	@Mandatory
+	@Valid
+	@OneToMany(mappedBy = "booking")
+	private List<Passenger>		passenger;
 }
