d825a5245b65bcf4670f586fd92de61524024f3f
celiasuaco <celiasuarezcoronel@gmail.com>
2025-03-08 17:31:19 +0100

    Adición de anotaciones

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -13,7 +12,9 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
 import acme.entities.airline.Airline;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,24 +32,29 @@ public class AssistanceAgent extends AbstractRole {
 	private String				employeeCode;
 
 	@Mandatory
-	@Size(max = 255) 
+	@Size(max = 255)
+	@Automapped
 	private String				languages;
 
-
 	@Mandatory
-	@ManyToOne
+	@Automapped
 	private Airline				airline;
 
 	@Mandatory
 	@Past
+	@Automapped
 	private LocalDate			startDate;
 
 	@Size(max = 255)
+	@Automapped
 	private String				bio;
 
+	@Optional
 	private Double				salary;
 
+	@Optional
 	@URL
+	@Automapped
 	private String				photoURL;
 
 }
