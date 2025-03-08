e0d75d3c481ee0c5f252157ea953f2b2baa4ab47
juagonluc1 <juagonluc1@alum.us.es>
2025-03-08 23:36:06 +0100

    fallo en el nombre

--- src/main/java/acme/entities/assigment/FlightAssignment.java
+++ src/main/java/acme/entities/assigment/FlightAssignment.java
@@ -25,7 +25,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class FlightAssigment extends AbstractEntity {
+public class FlightAssignment extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
 
