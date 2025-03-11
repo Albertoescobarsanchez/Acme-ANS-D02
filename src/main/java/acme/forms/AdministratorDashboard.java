2a30fcad07c2b507a471b71e6192eb17a8418cba
Olga Cantalejo <2004olgacg@gmail.com>
2025-03-11 10:02:18 +0100

    group-Task025

---
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -0,0 +1,32 @@
+package acme.forms;
+
+import java.util.Map;
+
+import acme.client.components.basis.AbstractForm;
+import acme.entities.airline.AirlineType;
+import acme.entities.airport.OperationalScope;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdministratorDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long				serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+    private Map<OperationalScope, Integer>	numberAirports;
+	private Map<AirlineType, Integer>		numberAirlines;
+	private Double							ratiowithEmailAndPhoneNumber;
+	private Double							ratioActiveAircraft;
+	private Double							ratioNonActiveAircraft;
+	private Double							reviewsWithScoreAbove5;
+	private Double							countNumberOfReviews;  //number of reviews posted over the last 10 weeks
+	private Double							averageNumberOfReviews;
+	private Double							minimumNumberOfReviews;
+	private Double							maximumNumberOfReviews;
+	private Double							standardDeviationNumberOfReviews;
+}
