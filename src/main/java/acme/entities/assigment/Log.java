044bb3aa4ea96b61ac6c2910dc9cef2c67b0333f
juagonluc1 <juagonluc1@alum.us.es>
2025-03-11 09:39:25 +0100

    fallo N

--- src/main/java/acme/entities/assigment/Log.java
+++ src/main/java/acme/entities/assigment/Log.java
@@ -35,7 +35,7 @@ public class Log extends AbstractEntity {
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
-	private FlightAssigment		assigment;
+	private FlightAssignment	assigment;
 
 	@Mandatory
 	@Temporal(TemporalType.TIMESTAMP)
